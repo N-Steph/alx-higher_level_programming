168a53a3b3cfe3cd0939da54037a9a4565fe548c
nouaffo <snouaffo@gmail.com>
2023-08-13 10:33:55 +0000

    use string format

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-print("abcdefghijklmnopqrstuvwxyz", end='')
-#for letter in "abcdefghijklmnopqrstuvwxyz":
-#    print(f"{letter}", end='')
+for letter in "abcdefghijklmnopqrstuvwxyz":
+    print("{}".format(letter), end='')
