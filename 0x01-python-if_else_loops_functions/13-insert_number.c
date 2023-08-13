83cf896c7162f32bcc90f468a0bd2611d03ad237
nouaffo <snouaffo@gmail.com>
2023-08-13 13:59:08 +0000

    "function that inserts a number into a sorted singly linked list"

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,51 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * insert_node - inserts a number into a sorted singly linked list
+ * @head: pointer to address of first node
+ * @number: number to insert in linked list
+ *
+ * Return: pointer of type listint_t
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *next_node, *prev_node;
+
+	if (head == NULL)
+		return (NULL);
+	if (*head == NULL)
+	{
+		new_node = malloc(sizeof(listint_t));
+		if (new_node == NULL)
+			return (NULL);
+		new_node->n = number;
+		*head = new_node;
+		new_node->next = NULL;
+		return (new_node);
+	}
+	next_node = *head;
+	prev_node = *head;
+	while (next_node != NULL)
+	{
+		if (next_node->n <= number)
+		{
+			new_node = malloc(sizeof(listint_t));/* Not freed */
+			if (new_node == NULL)
+				return (NULL);
+			new_node->n = number;
+			prev_node->next = new_node;
+			new_node->next = next_node;
+			return (new_node);
+		}
+		prev_node = next;
+		next_node = next->next
+	}
+	new_node = malloc(sizof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = number;
+	prev_node->next = new_node;
+	new_node->next = NULL;
+	return (new_node);
+}
