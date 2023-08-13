f437992838622910a657f0da1e2f6257094d9aa1
nouaffo <snouaffo@gmail.com>
2023-08-13 10:30:01 +0000

    Update

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for letter in "abcdefghijklmnopqrstuvwxyz":
-    print(f"{letter}", end='')
+print("abcdefghijklmnopqrstuvwxyz", end='')
+#for letter in "abcdefghijklmnopqrstuvwxyz":
+#    print(f"{letter}", end='')
