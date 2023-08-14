0525e5e730b7ca6a4f5c45409d98f2f5f92da7d1
nouaffo <snouaffo@gmail.com>
2023-08-14 17:24:42 +0000

    "add two tuples"

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    """
+    Adds 2 tuples.
+    Add the first element of tuple_a with
+    first element of tuple_b and
+    second element of tuple_a with second
+    element of tuple_b
+
+    Return a tuple of the two integers obtain from the addition
+    """
+    if len(tuple_a) >= 2 and len(tuple_b) >= 2:
+        tuple_add = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+    elif len(tuple_a) == 1 and len(tuple_b) == 2:
+        tuple_add = tuple_a[0] + tuple_b[0], 0 + tuple_b[1]
+    elif len(tuple_a) == 2 and len(tuple_b) == 1:
+        tuple_add = tuple_a[0] + tuple_b[0],  tuple_a[1] + 0
+    elif len(tuple_a) == 1 and len(tuple_b) == 1:
+        tuple_add = tuple_a[0] + tuple_b[0], 0
+    elif len(tuple_a) == 0 and len(tuple_b) == 1:
+        tuple_add = tuple_b[0], 0
+    elif len(tuple_a) == 1 and len(tuple_b) == 0:
+        tuple_add = tuple_a[0], 0
+    else:
+        tuple_add = 0, 0
+    return tuple_add
