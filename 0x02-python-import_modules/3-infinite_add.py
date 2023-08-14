31dc9d54cfa3bc2db1a9600203f4f1df27ea7acf
nouaffo <snouaffo@gmail.com>
2023-08-14 13:57:07 +0000

    "made the code not executable when imported"

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-from sys import argv
-result = 0
-if len(argv) != 1:
-    i = 1
-    while i < len(argv):
-        result += int(argv[i])
-        i += 1
+if __name__ == "__main__":
+    from sys import argv
+    result = 0
+    if len(argv) != 1:
+        i = 1
+        while i < len(argv):
+            result += int(argv[i])
+            i += 1
 
-print(result)
+    print(result)
