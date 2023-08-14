2819bcecbcb5ada9692d756937e36730d1977350
nouaffo <snouaffo@gmail.com>
2023-08-14 16:57:20 +0000

    "remove 'c' and 'C' from a string"

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    """
+    This function removes all characters 'c' and 'C' from 'my_string'
+    """
+    new_string = ""
+    for letter in my_string:
+        if letter != "c" and letter != "C":
+            new_string += letter
+    return new_string
