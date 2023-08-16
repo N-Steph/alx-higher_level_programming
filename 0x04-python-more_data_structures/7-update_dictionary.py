fe3a0dc53004d71dd2307dfa72c0f1d41368deaa
nouaffo <snouaffo@gmail.com>
2023-08-16 13:50:22 +0000

    "add return statement"

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -3,4 +3,6 @@ def update_dictionary(a_dictionary, key, value):
     """
     Replaces or adds key/value in a dictionary
     """
-    a_dictionary[key] = value
+    new_dict = a_dictionary.copy()
+    new_dict[key] = value
+    return new_dict
