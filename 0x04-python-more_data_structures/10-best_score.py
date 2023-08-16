49fdd52ac524e4aeb21fe85cef92183cc85cae1e
nouaffo <snouaffo@gmail.com>
2023-08-16 14:16:14 +0000

    "return a key with the biggest integer value"

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    """
+    Returns the key with the biggest integer value
+    """
+    if len(a_dictionary) == 0:
+        return None
+    biggest = a_dictionary[list(a_dictionary)[0]]
+    biggest_key = list(a_dictionary)[0]
+    for key, value in a_dictionary.items():
+        if value > biggest:
+            biggest = a_dictionary[key]
+            biggest_key = key
+    return biggest_key
