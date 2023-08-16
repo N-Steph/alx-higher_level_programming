c0e12990f1c05e812d6cb2d60dfe352a690670e0
nouaffo <snouaffo@gmail.com>
2023-08-16 14:03:16 +0000

    "Add if condition"

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -3,5 +3,6 @@ def simple_delete(a_dictionary, key=""):
     """
     Deletes a key from a dictionary
     """
-    del a_dictionary[key]
+    if key in a_dictionary:
+        del a_dictionary[key]
     return a_dictionary
