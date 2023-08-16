da29a1704a6c061ca95726aaa9a947e1cdf53271
nouaffo <snouaffo@gmail.com>
2023-08-16 13:42:51 +0000

    "Change output format"

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -7,4 +7,5 @@ def print_sorted_dictionary(a_dictionary):
     new_dict = {}
     for key in keys:
         new_dict[key] = a_dictionary[key]
-    print(new_dict)
+    for key, value in new_dict.items():
+        print("{}: {}".format(key, value))
