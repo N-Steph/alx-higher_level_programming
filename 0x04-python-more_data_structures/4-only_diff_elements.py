342b456d78996466cc932b0b40fe29e0c67440a3
nouaffo <snouaffo@gmail.com>
2023-08-16 13:18:46 +0000

    "Finding the symmetric-difference between sets"

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    """
+    Return the symmetric-difference of 'set_1' and 'set_2'
+    """
+    difference = set_1 ^ set_2
+    return difference
