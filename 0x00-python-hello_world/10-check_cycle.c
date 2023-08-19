22e2a7d46560097cdec1ea3511ccde58a5d034df
nouaffo <snouaffo@gmail.com>
2023-08-19 17:01:40 +0000

    change algorithm

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,26 +1,4 @@
 #include "lists.h"
-/**
- * listint_t_len - counts the number of node in a listint_t list
- * @list: pointer to head node of listint_t list
- *
- * Return: number of node
- */
-size_t listint_t_len(listint_t *list)
-{
-	size_t num_node;
-	listint_t *curr_node;
-
-	if (list == NULL)
-		return (0);
-	curr_node = list;
-	num_node = 0;
-	while (curr_node != NULL)
-	{
-		++num_node;
-		curr_node = curr_node->next;
-	}
-	return (num_node);
-}
 
 /**
  * check_cycle - Checks if a singly linked list has a cycle
@@ -30,17 +8,16 @@ size_t listint_t_len(listint_t *list)
  */
 int check_cycle(listint_t *list)
 {
-	size_t num; /* number of nodes */
-	size_t i;
 	listint_t *node;
 
 	if (list == NULL)
 		return (0);
-	num = listint_t_len(list);
 	node = list;
-	for (i = 0; i < num; i++)
+	while (node != NULL)
+	{
 		node = node->next;
-	if (node->next == list)
-		return (1);
+		if (node == list)
+			return (1);
+	}
 	return (0);
 }
