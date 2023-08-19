32024086c8b5bc8dec74e3fac0c7804078522d82
nouaffo <snouaffo@gmail.com>
2023-08-19 16:26:05 +0000

    Added some function prototypes

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,6 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -17,4 +19,9 @@ typedef struct listint_s
 
 int check_cycle(listint_t *list);
 size_t listint_t_len(listint_t *list);
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
+
 #endif /* LISTS_H */
