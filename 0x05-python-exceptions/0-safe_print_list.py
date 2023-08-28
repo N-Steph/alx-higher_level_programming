63a5077c78ddc59ef98ec7b26e7d04ad31c50c1e
nouaffo <snouaffo@gmail.com>
2023-08-28 18:19:38 +0000

    Add else clause and new variable

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     i = 0
+    counter = 0
     while (i < x):
         try:
             print(my_list[i], end="")
         except IndexError:
-            print("Index out of bound")
+            i += 1
+            pass
+        else:
+            counter += 1
+            i += 1
+    print()
+    return (counter)
