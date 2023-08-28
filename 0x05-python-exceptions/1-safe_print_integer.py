9836994b14ea2a60bf1d10e5b14b227cece2e210
nouaffo <snouaffo@gmail.com>
2023-08-28 18:29:30 +0000

    function that prints an integer with '{:d}'.format()

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    """
+    Function that prints an integer with '{:d}".format()
+    """
+    try:
+        print("{:d}".format(value))
+    except ValueError:
+        return (False)
+    else:
+        return (True)
