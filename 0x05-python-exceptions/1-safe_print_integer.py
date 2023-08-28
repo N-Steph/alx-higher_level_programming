6d7609bf735206bce0f308bb2be5cbe684b1b160
nouaffo <snouaffo@gmail.com>
2023-08-28 18:46:21 +0000

    Remove exception name after except keyword

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,10 +2,11 @@
 def safe_print_integer(value):
     """
     Function that prints an integer with '{:d}".format()
+    Returns Ture if value correctly printed and false otherwise
     """
     try:
         print("{:d}".format(value))
-    except ValueError:
+    except:
         return (False)
     else:
         return (True)
