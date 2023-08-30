f843ae938e040089de6e778db9c9514e89965c89
nouaffo <snouaffo@gmail.com>
2023-08-30 02:49:54 +0000

    Added module documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""Square class module.
+This module defines a class called Square with some attributes.
+"""
+
+
 class Square:
     """Create and empty class Square"""
     pass
