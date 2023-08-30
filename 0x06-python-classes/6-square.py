b6a33197fbde8a10fcf4f755ee63fa783d124fca
nouaffo <snouaffo@gmail.com>
2023-08-30 11:22:52 +0000

    Added another condition in the if statement

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -52,6 +52,7 @@ class Square:
         if not (type(value) == tuple and
                 type(value[0]) == int and
                 type(value[1]) == int and
+                len(value) == 2 and
                 value[0] >= 0 and value[1] >= 0):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
