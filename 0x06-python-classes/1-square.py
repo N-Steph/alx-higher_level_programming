bd64fbd9871cb915ae2e8ef4b1470450957bdb04
nouaffo <snouaffo@gmail.com>
2023-08-30 00:44:30 +0000

    Added an __init__ method to the Square class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+class Square:
+    """Defines Square class"""
+
+    def __init__(self, size):
+        """Initialize Square object
+        Args:
+            size: size of the Square object
+        """
+        self.__size = size
