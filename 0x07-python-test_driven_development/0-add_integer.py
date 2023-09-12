dc0b440ababa5b9fbc8d3ef5cfa568985f0fa196
nouaffo <snouaffo@gmail.com>
2023-09-12 10:08:23 +0000

    Update

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -8,7 +8,7 @@ def add_integer(a, b=98):
     arguments = [a, b]
     arguments = {"a": a, "b": b}
     for key, value in arguments.items():
-        if value is None or type(value) not in num_type or value == NaN:
+        if value is None or type(value) not in num_type or value == "NaN":
             raise TypeError("{} must be an integer".format(key))
     if type(a) is num_type[1] or type(b) is num_type[1]:
         try:
