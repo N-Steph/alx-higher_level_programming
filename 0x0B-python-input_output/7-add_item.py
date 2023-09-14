7d3cd066c7cd88b3d0d6f4d13e5a2e9f0dec3604
nouaffo <snouaffo@gmail.com>
2023-09-14 09:42:41 +0000

    "adds all arguments to a python list"

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This script adds all arguments to a Python list and then
+save them to a file
+"""
+
+import sys
+import json
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+try:
+    des_list = load_from_json_file('add_item.json')
+except FileNotFoundError:
+    with open('add_item.json', mode='w', encoding='utf-8') as file_open:
+        json.dump([], file_open)
+    sys.exit()
+for i in range(1, len(sys.argv)):
+    des_list.append(sys.arv[i])
+save_to_json_file(des_list, 'add_item.json')
