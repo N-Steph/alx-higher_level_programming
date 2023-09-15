d145cfab2b81b1f5ac5a6aa4d56848f46d90afc7
nouaffo <snouaffo@gmail.com>
2023-09-15 14:02:52 +0000

    "Added another condition"

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -9,7 +9,7 @@ def print_square(size):
     """
     Print as square using '#' symbol of size 'size'
     """
-    if type(size) is not int:
+    if type(size) is not int or (type(size) is float and size < 0):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
