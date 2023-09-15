75e959f0d2c6f3573277cdf73acae99396cfb4c9
nouaffo <snouaffo@gmail.com>
2023-09-15 13:18:31 +0000

    "Added documentation"

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""
+This module contains the definition of the function
+say_my_name
+"""
+
+
 def say_my_name(first_name, last_name=""):
+    """
+    Prints the a string to the stdout containing the first_name
+    and last_name argument passed to it.
+    """
     data = {"first_name": first_name, "last_name": last_name}
     for key, value in data.items():
         if type(value) is not str:
