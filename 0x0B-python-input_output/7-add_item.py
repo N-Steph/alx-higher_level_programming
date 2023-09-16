461cb79b85847ddf8dccf354607cd97580992843
nouaffo <snouaffo@gmail.com>
2023-09-16 11:54:06 +0000

    "Update"

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -15,8 +15,11 @@ try:
     des_list = load_from_json_file('add_item.json')
 except FileNotFoundError:
     with open('add_item.json', mode='w', encoding='utf-8') as file_open:
-        json.dump([], file_open)
+        _list = []
+        for i in range(1, len(sys.argv)):
+            _list.append(sys.argv[i])
+        json.dump(_list, file_open)
     sys.exit()
 for i in range(1, len(sys.argv)):
-    des_list.append(sys.arv[i])
+    des_list.append(sys.argv[i])
 save_to_json_file(des_list, 'add_item.json')
