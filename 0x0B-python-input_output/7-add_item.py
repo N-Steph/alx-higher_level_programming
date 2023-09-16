524a49f6195d0b93012f2cf129d731e2ec30679f
nouaffo <snouaffo@gmail.com>
2023-09-16 16:34:44 +0000

    "put code in function"

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -11,15 +11,20 @@ import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    des_list = load_from_json_file('add_item.json')
-except FileNotFoundError:
-    with open('add_item.json', mode='w', encoding='utf-8') as file_open:
-        _list = []
-        for i in range(1, len(sys.argv)):
-            _list.append(sys.argv[i])
-        json.dump(_list, file_open)
-    sys.exit()
-for i in range(1, len(sys.argv)):
-    des_list.append(sys.argv[i])
-save_to_json_file(des_list, 'add_item.json')
+def add_item(filename, my_list):
+    """Add items to a file"""
+    try:
+        des_list = load_from_json_file(filename)
+    except FileNotFoundError:
+        with open(filename, mode='w', encoding='utf-8') as file_open:
+            _list = []
+            for i in range(1, len(my_list)):
+                _list.append(my_list[i])
+            json.dump(_list, file_open)
+        sys.exit()
+    for i in range(1, len(my_list)):
+        des_list.append(my_list[i])
+    save_to_json_file(des_list, 'add_item.json')
+
+if __name__ == "__main__":
+    add_item('add_item.json', sys.argv)
