a3dd18cafea3d25570591c12d09b5eb194f1ebda
nouaffo <snouaffo@gmail.com>
2023-09-17 17:40:40 +0000

    "add documentation"

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""Definition of add_attribute function"""
+
+
 def add_attribute(obj, attr, value):
+    """Adds an attribute 'attr' to object 'object' with value 'value'.
+    raise TypeError exception if we can't add attribute 'attr'
+    """
     object_type = [int, float, str, bool, list, set, tuple, None]
     if type(obj) in object_type:
         raise TypeError("can't add new attribute")
