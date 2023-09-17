f1e3a7df7bca92d0c0232bbc26be76573366db27
nouaffo <snouaffo@gmail.com>
2023-09-17 02:23:12 +0000

    Added module documentation

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""This module defines a class that can accept only one predefined
+attribute
+"""
+
+
 class LockedClass:
     """Predefining attributes for instances of a class"""
     __slots__ = ("first_name",)
