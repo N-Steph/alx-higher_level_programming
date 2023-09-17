bee9648c2402332e5746740de8f44759d3121f5c
nouaffo <snouaffo@gmail.com>
2023-09-17 16:31:34 +0000

    "implement Square class"

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Implementation of the Square class"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class implementation
+    Square class inherits from Rectangle class which iself inherits\
+     from the BaseGeometry class
+    Here we have a case of multi inheritance
+    """
+
+    def __init__(self, size):
+        """Initialze the Square instance.
+        self.__size: instance variable, size of the square
+        """
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """Returns the area of a Square instance"""
+        return self.__size * self.__size
