2d006d5cf23eb068fcfa3a2b2ef2303cd76abb57
nouaffo <snouaffo@gmail.com>
2023-09-17 18:06:08 +0000

    "Update"

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -7,9 +7,13 @@ def add_attribute(obj, attr, value):
     raise TypeError exception if we can't add attribute 'attr'
     """
     object_type = [int, float, str, bool, list, set, tuple, None]
+    attr_exception = TypeError("can't add new attribute")
     if type(obj) in object_type:
-        raise TypeError("can't add new attribute")
+        raise attr_exception 
     if 'slots' in dir(obj):
         if attr not in obj.slots:
-            raise TypeError("can't add new attribute")
+            raise attr_exception 
+    if '__slots__' in dir(obj):
+        if attr not in obj.__slots__:
+            raise attr_exception 
     obj.__dict__[attr] = value
