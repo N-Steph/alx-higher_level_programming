0e0d8cd7c9f7b1908c38c876262cf003d8895e3c
nouaffo <snouaffo@gmail.com>
2023-09-18 21:34:04 +0000

    added update() method

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -15,12 +15,39 @@ class Square(Rectangle):
 
     @property
     def size(self):
+        """getter for size attribute"""
         return self.width
 
     @size.setter
     def size(self, value):
+        """setter for size attribute"""
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """Update attributes of Square intance"""
+        if len(args) != 0:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.width = args[1]
+                self.height = args[1]
+            if len(args) >= 4:
+                self.x = args[3]
+            if len(args) >= 5:
+                self.y = args[4]
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                if key == "size":
+                    self.width = value
+                if key == "size":
+                    self.height = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = value
 
     def __str__(self):
         """Returns the string representation of Square instance"""
