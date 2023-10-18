7999cbcff5f5cd3d35e319f8b9eb27d38e388915
N-Steph <snouaffo@gmail.com>
2023-10-18 15:04:32 +0000

    calculating average

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,4 @@
+-- This script computes the score average of all records in second_table
+-- of the database hbtn_0c_0
+SELECT AVG(id) AS average FROM second_table;
+-- SELECT (SUM(id) / COUNT(*)) AS average FROM second_table;
