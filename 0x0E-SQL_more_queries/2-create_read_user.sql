b36664d9938d31dea33cb431b951a43420cd9955
N-Steph <snouaffo@gmail.com>
2023-10-21 21:59:10 +0000

    corrected database name

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,4 +4,4 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- create user user_0d_2
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- grant SELECT privilege to user_0d_2
-GRANT SELECT ON htbn_0d_2.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
