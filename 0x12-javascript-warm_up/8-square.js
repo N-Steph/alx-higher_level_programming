b4b5009ab4441b2aded2d7f6208b8805880a0cb9
N-Steph <snouaffo@gmail.com>
2023-11-15 13:02:28 +0000

    nesting loops in javascript

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+const { argv } = require('node:process');
+const size = parseInt(argv[2]);
+if (argv[2] === undefined || isNaN(size)) {
+  console.log('Missing size');
+} else if (size > 0) {
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      process.stdout.write('X');
+    }
+    process.stdout.write('\n');
+  }
+}
