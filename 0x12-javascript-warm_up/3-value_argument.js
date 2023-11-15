d923596ec63e4ab2017e781a6557e66806c57087
N-Steph <snouaffo@gmail.com>
2023-11-15 12:56:45 +0000

    printing first command line argument

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+const { argv } = require('node:process');
+for (let i = 0; i < 2; i++) {
+  argv.shift();
+}
+if (argv[0] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(argv[0]);
+}
