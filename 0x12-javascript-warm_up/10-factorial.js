dce0b0c604b7aa87bf8cd1fa93bc795e3102271e
N-Steph <snouaffo@gmail.com>
2023-11-15 13:03:40 +0000

    recursive function in javascript

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+const { argv } = require('node:process');
+function factorial (number) {
+  if (number <= 0) {
+    return 1;
+  }
+  return number * factorial(number - 1);
+}
+
+const argument = parseInt(argv[2]);
+if (argument === undefined || isNaN(argument)) {
+  console.log(1);
+} else {
+  const answer = factorial(argument);
+  console.log(answer);
+}
