8b2aace461bae546d639ece69cecc69a47fa7e64
N-Steph <snouaffo@gmail.com>
2023-11-15 13:03:06 +0000

    defining a function in javascript

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+function add (a, b) {
+  const result = a + b;
+  console.log(result);
+}
+const { argv } = require('node:process');
+if (argv.length > 2 && argv[2] !== undefined && argv[3] !== undefined &&
+    !isNaN(parseInt(argv[2])) && !isNaN(parseInt(argv[3]))) {
+  const a = parseInt(argv[2]);
+  const b = parseInt(argv[3]);
+  add(a, b);
+} else {
+  console.log('NaN');
+}
