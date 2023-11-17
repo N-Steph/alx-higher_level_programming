f962001052a828175f05dc17b5e8875091d98e81
N-Steph <snouaffo@gmail.com>
2023-11-17 02:29:17 +0000

    reversing elements of a list

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+  let reverseList = [];
+  if (list.length !== 0) {
+    for (let i = list.length - 1; i >= 0; i--) {
+      reverseList.push(list[i]);
+    }
+  }
+  return reverseList;
+};
