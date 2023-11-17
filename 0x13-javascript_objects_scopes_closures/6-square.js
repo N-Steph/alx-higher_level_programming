3b75f8930f5cc5f3dacfddce64f31e8e60a528ad
N-Steph <snouaffo@gmail.com>
2023-11-17 02:02:46 +0000

    added the charPrint() method to the Square class

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,48 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X');
+      }
+      process.stdout.write('\n');
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === 'X' || c === undefined) {
+      super.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        for (let j = 0; j < this.width; j++) {
+          process.stdout.write(c);
+        }
+        process.stdout.write('\n');
+      }
+    }
+  }
+};
