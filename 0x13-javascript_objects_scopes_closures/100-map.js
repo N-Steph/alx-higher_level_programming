694ebf0645f31d98f5ee584353a64490cfaf4df2
N-Steph <snouaffo@gmail.com>
2023-11-17 11:37:53 +0000

    update

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,12 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
 
-const list2 = list.map((x) => x * list.indexOf(x));
+const list2 = list.map((x, index) => {
+  if (index === 0 && x < 0) {
+    return x * -1 * index;
+  } else {
+    return x * index
+  }
+});
 console.log(list);
 console.log(list2);
