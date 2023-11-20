229939a2efd3e4d96aba8ac025dde54abbd65761
N-Steph <snouaffo@gmail.com>
2023-11-20 18:34:45 +0000

    add BINARY keyword for case-sensitivity

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
     db_connect = connect(user=argv[1], password=argv[2], db=argv[3],
                          host="localhost", port=3306)
     cur = db_connect.cursor()
-    cur.execute("SELECT id, name 
-                 FROM states WHERE name LIKE BINARY'N%'")
+    cur.execute("SELECT id, name
+                FROM states WHERE name LIKE BINARY'N%'")
     rows = cur.fetchall()
     for row in rows:
         print(row)
