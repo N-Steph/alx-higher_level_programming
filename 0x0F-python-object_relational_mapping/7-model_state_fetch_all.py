3402d89d242edce692a1ddc430307a2f2cca2c5d
N-Steph <snouaffo@gmail.com>
2023-11-21 13:53:10 +0000

    selecting records from the state table in hbtn_0e_6_usa database

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Script listing all State objects from the database hbtn_0e_6_usa"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
+
+if __name__ == "__main__":
+    # first step of connecting to a database
+    # Create an engine
+    data_source_inf = "mysql+mysqldb://root:root@localhost:3306/hbtn_0e_6_usa"
+    engine = create_engine(data_source_inf)
+    # connecting the database
+    engine.connect()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states_list = session.query(State).order_by(State.id).all()
+    for state in states_list:
+        print(str(state.id) + ": " + state.name)
