4710de8118b198cb56c5f62d84aa4f5fca655b52
N-Steph <snouaffo@gmail.com>
2023-11-23 12:58:55 +0000

    removed the if condition

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,11 +2,7 @@
 const list = require('./100-data').list;
 
 const list2 = list.map((x, index) => {
-  if (index === 0 && x < 0) {
-    return x * -1 * index;
-  } else {
     return x * index;
-  }
 });
 console.log(list);
 console.log(list2);
