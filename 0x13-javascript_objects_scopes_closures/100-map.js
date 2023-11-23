3b6b7360949c151521baf74ad76dd0f811bfd0cc
N-Steph <snouaffo@gmail.com>
2023-11-23 12:53:22 +0000

    added semicolon

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -5,7 +5,7 @@ const list2 = list.map((x, index) => {
   if (index === 0 && x < 0) {
     return x * -1 * index;
   } else {
-    return x * index
+    return x * index;
   }
 });
 console.log(list);
