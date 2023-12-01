6dcb6570668ba5fed46c29c30fae37057fc3719e
N-Steph <snouaffo@gmail.com>
2023-11-30 18:13:41 -0800

    refactor the code to extract the content-length value of the reponse header

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a request to a URL and displays the size of the body of the response
-curl $1 -s --head | tail -n 1
+curl $1 -s --head | tail -n 2 | cut -d " " -f 2 | head -n 1 
