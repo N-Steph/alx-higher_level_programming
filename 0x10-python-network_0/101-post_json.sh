13d82d9d8e01e6dfb907ec25141ea5da381021e3
N-Steph <snouaffo@gmail.com>
2023-12-04 09:18:53 -0800

    added @ before variable containing the filename

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a JSON POST request and displays body of the response
-curl -X POST $1 -H "Content-Type: application/json" -d $2
+curl -X POST $1 -H "Content-Type: application/json" -d @$2
