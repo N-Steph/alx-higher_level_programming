338b2aabcd16fcbba041a7554f5ba0c84a7f3097
N-Steph <119834045+N-Steph@users.noreply.github.com>
2024-01-08 11:04:39 +0100

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/7-multi_c.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const argv = process.argv;
-if (argv[2] === undefined || isNaN(parseInt(argv[2]))) {
-  console.log('Missing number of occurences');
-} else if (parseInt(argv[2]) > 0) {
-  for (let i = 0; i < parseInt(argv[2]); i++) {
-    console.log('C is fun');
-  }
-}
--- 0x12-javascript-warm_up/README.md
+++
@@ -1,2 +0,0 @@
-## Description
-In this directory we are going look at how JavaScript is amazing.
