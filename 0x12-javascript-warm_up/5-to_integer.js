a7f5c59f6f9065353d884f27aa5570cc192c99fd
N-Steph <snouaffo@gmail.com>
2024-01-09 11:10:54 +0000

    removed line 2

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
 
-const number = parseInt(argv[2], 10);
+const number = parseInt(process.argv[2], 10);
 
 if (isNaN(number)) {
   console.log('Not a number');
