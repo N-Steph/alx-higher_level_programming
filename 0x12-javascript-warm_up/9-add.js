567d49c27fb0e85dc0783bbac371fd441fb06904
N-Steph <snouaffo@gmail.com>
2024-01-09 11:16:39 +0000

    deleted line 2

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
 
 function add (a, b) {
   const result = a + b;
   console.log(result);
 }
 
-add(parseInt(argv[2], 10), parseInt(argv[3]));
+add(parseInt(process.argv[2], 10), parseInt(process.argv[3]));
