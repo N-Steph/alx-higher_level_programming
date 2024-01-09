c7fb5a60edc20ff8ef5cbd80eaa2934009f108ec
N-Steph <snouaffo@gmail.com>
2024-01-09 11:05:19 +0000

    remove second line and updated the call of command line array

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 
-const { argv } = require('node:process');
-
-if (typeof(argv[2]) === 'undefined') {
+if (typeof process.argv[2] === 'undefined') {
   console.log('No argument');
 } else {
-  console.log(argv[2]);
+  console.log(process.argv[2]);
 }
