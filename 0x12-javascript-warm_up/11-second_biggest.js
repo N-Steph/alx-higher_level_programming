e9173ac51de7927d79f78b30d7a0a6df23be16ea
N-Steph <snouaffo@gmail.com>
2024-01-09 12:08:55 +0000

    refactor code

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,17 +2,26 @@
 
 if (process.argv.length === 2 || process.argv.length === 3) {
   console.log(0);
+} else if (process.argv.length === 4) {
+  if (parseInt(process.argv[2]) > parseInt(process.argv[3])) {
+    console.log(parseInt(process.argv[3]));
+  } else {
+    console.log(parseInt(process.argv[2]));
+  }
 } else {
   let biggest = parseInt(process.argv[2], 10);
   let secondBiggest = biggest;
   for (let i = 3; i < process.argv.length; i++) {
     const number = parseInt(process.argv[i], 10);
     if (number > biggest) {
-      secondBiggest = biggest;
       biggest = number;
+    }
+  }
+  for (let i = 3; i < process.argv.length; i++) {
+    const number = parseInt(process.argv[i], 10);
+    if (number === biggest) {
+      continue;
     } else if (number > secondBiggest) {
-      secondBiggest = number;
-    } else if (number < biggest) {
       secondBiggest = number;
     }
   }
