de443d67b57bc9e1601880eddaf585829ecded69
N-Steph <snouaffo@gmail.com>
2024-01-09 11:24:47 +0000

    deleted line 2

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
 
-if (argv.length === 2 || argv.length === 3) {
+if (process.argv.length === 2 || process.argv.length === 3) {
   console.log(0);
 } else {
-  let biggest = parseInt(argv[2], 10);
+  let biggest = parseInt(process.argv[2], 10);
   let secondBiggest = biggest;
-  for (let i = 3; i < argv.length; i++) {
-    const number = parseInt(argv[i], 10);
+  for (let i = 3; i < process.argv.length; i++) {
+    const number = parseInt(process.argv[i], 10);
     if (number > biggest) {
       secondBiggest = biggest;
       biggest = number;
