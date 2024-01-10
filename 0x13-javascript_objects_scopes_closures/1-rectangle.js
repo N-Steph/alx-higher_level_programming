bd5da12219ae95ae8ae409e8829c12996045adde
N-Steph <snouaffo@gmail.com>
2024-01-10 02:48:02 +0000

    update

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -4,8 +4,8 @@ module.exports = class Rectangle {
   width;
   height;
 
-  constructor (width, height) {
-    this.width = width;
-    this.height = height;
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
   }
 };
