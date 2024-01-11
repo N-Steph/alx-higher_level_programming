2cb0cbbdbdc3c328b9be3b6979a491678d395c61
N-Steph <snouaffo@gmail.com>
2024-01-11 19:05:02 +0000

    reading from and writing to file

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+// Reading first file
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+  if (err) throw err;
+  // Reading second file
+  fs.readFile(process.argv[3], 'utf-8', (err, data1) => {
+    if (err) throw err;
+    const value = data.toString() + '\n' + data1.toString();
+    // writing the content of first and second file to a third
+    fs.writeFile(process.argv[4], value, (err) => {
+      if (err) throw err;
+      console.log('The file has been saved!');
+    });
+  });
+});
