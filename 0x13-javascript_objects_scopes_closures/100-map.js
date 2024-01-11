a31ea566146aeb2e53b559a94325639fb513a2b9
N-Steph <snouaffo@gmail.com>
2024-01-11 13:39:20 +0000

    refactor

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
 
-const newList = list.map((x) => x * list.indexOf(x));
+let i = -1;
+const newList = list.map((x) => x * list.indexOf(x, i + 1));
 console.log(list);
 console.log(newList);
