530c4299c4e576208fcdcb7fb8649c1961a7981e
N-Steph <snouaffo@gmail.com>
2024-01-11 13:49:56 +0000

    defined the mapping function

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,12 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
 
-let i = -1;
-const newList = list.map((x) => x * list.indexOf(x, i + 1));
+let i = 0;
+function index (x) {
+  const result = x * i;
+  i += 1;
+  return result;
+}
+const newList = list.map(index);
 console.log(list);
 console.log(newList);
