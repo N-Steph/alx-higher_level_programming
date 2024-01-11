9120cb62dab708ee4bc0cd15224c474d24b20c76
N-Steph <snouaffo@gmail.com>
2024-01-11 19:11:56 +0000

    removed a newline

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -7,7 +7,7 @@ fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   // Reading second file
   fs.readFile(process.argv[3], 'utf-8', (err, data1) => {
     if (err) throw err;
-    const value = data.toString() + '\n' + data1.toString();
+    const value = data.toString() + data1.toString();
     // writing the content of first and second file to a third
     fs.writeFile(process.argv[4], value, (err) => {
       if (err) throw err;
