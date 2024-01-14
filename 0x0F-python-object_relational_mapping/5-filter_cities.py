056b19579512b36ea635bc4922c8beecc1e4b643
N-Steph <snouaffo@gmail.com>
2024-01-14 06:48:00 +0000

    listing all cities in a state, from hbtn_0e_4_usa database

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+This script lists out all the cities found in a state from hbtn_0e_4_usa db
+The state name is passed as a command line argument.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    db = MySQLdb.connect('localhost', sys.argv[1], sys.argv[2], sys.argv[3])
+    cur = db.cursor()
+    query = """SELECT cities.name \
+               FROM cities \
+               WHERE cities.state_id = (\
+               SELECT states.id \
+               FROM states \
+               WHERE states.name = %s)"""
+    cur.execute(query, (sys.argv[4],))
+    rows = cur.fetchall()
+    counter = 1
+    for row in rows:
+        if counter < 5:
+            print(row[0] + ', ', end="")
+        else:
+            print(row[0])
+        counter += 1
+    cur.close()
+    db.close()
