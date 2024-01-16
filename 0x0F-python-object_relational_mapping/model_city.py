87075c508a3de9dc7d7f7423576c4484442c0cf1
N-Steph <snouaffo@gmail.com>
2024-01-16 18:57:51 +0000

    class definition of City

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+This script contains the model of City table
+"""
+
+from sqlalchemy import Column, Integer, String,\
+    PrimaryKeyConstraint, UniqueConstraint,\
+    ForeignKeyConstraint
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """Model of City table
+    attribute: id, name, state_id
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, nullable=False)
+
+    __table_args__ = (
+        PrimaryKeyConstraint('id'),
+        UniqueConstraint('id'),
+        ForeignKeyConstraint(['state_id'], ['states.id'])
+        )
