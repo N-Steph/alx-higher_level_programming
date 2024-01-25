a16d3fde647f9f2f70304699d0a3bfb15c7a7434
N-Steph <snouaffo@gmail.com>
2024-01-25 19:44:36 +0000

    added the -X option

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Display the size of the body of a response message after sending a request
-curl GET "$1" HTTP/1.1 | grep Content-Length | cut -d " " -f 2
+curl -X GET "$1" HTTP/1.1 | grep Content-Length | cut -d " " -f 2
