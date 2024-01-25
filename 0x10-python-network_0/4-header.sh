68da46c4c308c13bfeba0e3ca0d0ad7fd8dd44d9
N-Steph <snouaffo@gmail.com>
2024-01-25 21:39:37 +0000

    update

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Display body of a response message after sending a GET request with some data
-curl -sL --http1.1 --url "$1?X-School-User-Id=98"
+curl -sL --http1.1 --url "$1/?X-School-User-Id=98"
