4487b59b0d68955ecac12bbeadec1ef1582b92dc
N-Steph <snouaffo@gmail.com>
2024-01-25 22:12:55 +0000

    adding extra header variable to a GET request

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Display body of a response message after sending a GET request with some data
-curl -sLG --http1.1 --url "$1" --data X-School-User-Id=98
+curl -sL --http1.1 -H "X-School-User-Id: 98" --url "$1"
