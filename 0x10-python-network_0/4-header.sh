bf01fb6a85574e3bc11fd6fdc28b17e254b8751b
N-Steph <snouaffo@gmail.com>
2024-01-25 21:08:18 +0000

    updated the url

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Display body of a response message after sending a GET request with some data
-curl -sL --http1.1 --data X-School-User-Id=98 --url "$1"
+curl -sL --http1.1 --url "$1?X-School-User-Id=98"
