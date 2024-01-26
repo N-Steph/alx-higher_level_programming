211f4520c0ed331ea5e169d80cd5ff11e612acac
N-Steph <snouaffo@gmail.com>
2024-01-26 22:33:57 +0000

    requesting 'https://alx-intranet.hbtn.io/status' using urllib library

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+Script that fetches 'https://alx-intranet.hbtn.io/status'
+"""
+
+from urllib.request import urlopen
+
+
+if __name__ == '__main__':
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urlopen(url) as response:
+        response_body = response.read()
+        print("Body response:")
+        print("    - type: {}".format(type(response_body)))
+        print("    - content: {}".format(response_body))
+        print("    - utf8 content: {}".format(response.decode('utf-8')))
