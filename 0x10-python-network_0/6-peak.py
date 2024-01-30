a0469d0b59bd259e5c9c315ea1e536f2ccfa6e7c
N-Steph <snouaffo@gmail.com>
2024-01-30 21:38:04 +0000

    changed the algorithm

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,18 +5,23 @@ This script contains the defintion of find_peak function
 """
 
 
-def find_peak(list_of_integers):
+def find_peak(your_list):
     """Finds a peak in a list of unsorted integers"""
-    if len(list_of_integers) == 0:
+    len_list = len(your_list)
+    if len_list == 0:
         return
-    peak_element = list_of_integers[0]
-    i = 0
-    for elm in list_of_integers:
-        if i == 0:
-            i += 1
+    for i in range(1, len_list, 2):
+        if your_list[i - 1] < your_list[i] and your_list[i + 1] < your_list[i]:
+            peak_element = your_list[i]
+            return peak_element
+    for i in range(0, len_list, 2):
+        if i == 0 or i == len_list - 1:
             continue
-        if list_of_integers[i - 1] < elm and list_of_integers[i + 1 ] < elm:
-            peak_element = elm
-            break
-        i += 1
+        if your_list[i - 1] < your_list[i] and your_list[i + 1] < your_list[i]:
+            peak_element = your_list[i]
+            return peak_element
+    if your_list[0] >= your_list[len_list - 1]:
+        peak_element = your_list[0]
+    else:
+        peak_element = your_list[len_list - 1]
     return peak_element
