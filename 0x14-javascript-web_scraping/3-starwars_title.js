ac591610115dec11324610f6f1c0325a5d36df64
N-Steph <snouaffo@gmail.com>
2024-02-21 01:55:03 +0000

    refactor

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,10 +4,8 @@ const request = require('request');
 
 const starWarUrl = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 request(starWarUrl, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    const myData = JSON.parse(body);
-    console.log(myData.title);
+  if (!error && response.statusCode === 200) {
+    const info = JSON.parse(body);
+    console.log(info.title);
   }
 });
