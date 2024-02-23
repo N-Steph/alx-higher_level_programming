9574cbab56f1ed3d30cea16d5186f7d42f36e3ca
N-Steph <snouaffo@gmail.com>
2024-02-23 14:05:57 +0000

    added character set for name column

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,9 +1,9 @@
 -- This script converts hbtn_0c_0 database to UTF8
 -- connect to hbtn_0c_0 database
-connect hbtn_0c_0
+connect hbtn_0c_0;
 -- convert hbtn_0c_0 to UTF8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- convert first_table to UTF8
 ALTER TABLE first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- convert name column to UTF8
-ALTER TABLE first_table MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
