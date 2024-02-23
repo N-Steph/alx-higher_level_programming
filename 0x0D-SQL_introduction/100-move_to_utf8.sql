170d5b0aae8ac171c9bfe85f036e3094d6a34e12
N-Steph <snouaffo@gmail.com>
2024-02-23 21:59:02 +0000

    change collation of table

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,2 @@
 -- This script converts hbtn_0c_0 database to UTF8
--- create database
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0 COLLATE utf8mb4_unicode_ci;
--- create table
-CREATE TABLE IF NOT EXISTS first_table(id INT, name VARCHAR(256);
+ALTER DATABASE hbtn_0c_0 COLLATE utf8mb4_unicode_ci;
