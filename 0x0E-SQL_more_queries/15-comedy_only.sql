efef2b00ed2d3c32678c826299ffa9197a018c95
N-Steph <snouaffo@gmail.com>
2024-02-25 11:29:04 +0000

    change sql keyword to uppercase

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,3 +1,3 @@
 -- Script that lists all comedy shows in the database hbtn_0d_tvshows
-SELECT tv_shows.title from tv_shows, tv_genres, tv_show_genres WHERE tv_genres.id = tv_show_genres.genre_id AND tv_show_genres.show_id = tv_shows.id AND tv_genres.name = "Comedy" ORDER BY tv_shows.title;
+SELECT tv_shows.title FROM tv_shows, tv_genres, tv_show_genres WHERE tv_genres.id = tv_show_genres.genre_id AND tv_show_genres.show_id = tv_shows.id AND tv_genres.name = "Comedy" ORDER BY tv_shows.title;
 
