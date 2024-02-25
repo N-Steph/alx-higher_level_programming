b2bcfa23ef261f47dadfd2e625faea03ff131cb8
N-Steph <snouaffo@gmail.com>
2024-02-25 12:17:20 +0000

    updated the ORDER BY part of the query

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,2 +1,2 @@
 -- Script that lists all shows, and all genres linked to show from the database hbtn_0d_tvshows
-SELECT tv_shows.title, tv_genres.name FROM tv_show_genres INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id RIGHT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title;
+SELECT tv_shows.title, tv_genres.name FROM tv_show_genres INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id RIGHT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title, tv_genres.name;
