52d93c14b81c45ab2604dcee798ecf53d12509d4
N-Steph <snouaffo@gmail.com>
2024-02-25 02:32:44 +0000

    counting the number of films associated to a each genre

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,2 @@
+-- Script that lists all genres from hbtn_0d_tvshows and dispaly the number of shows linked to each
+SELECT tv_genres.name genre, COUNT(tv_show_genres.show_id) number_of_shows FROM tv_genres INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id GROUP BY genre ORDER BY number_of_shows DESC;
