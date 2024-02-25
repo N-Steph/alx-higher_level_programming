1457dab1d25120208a23f66d2397804c28c51bc3
N-Steph <snouaffo@gmail.com>
2024-02-25 11:50:08 +0000

    changed the entire query

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,3 +1,2 @@
--- Script that lists all comedy shows in the database hbtn_0d_tvshows
-SELECT tv_shows.title FROM tv_shows, tv_genres, tv_show_genres WHERE tv_genres.id = tv_show_genres.genre_id AND tv_show_genres.show_id = tv_shows.id AND tv_genres.name = "Comedy" ORDER BY tv_shows.title;
-
+-- Script that lists all comedy shows in the database hbtn_0d_tv_shows
+SELECT tv_shows.title FROM tv_show_genres INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id WHERE tv_genres.name = "Comedy" ORDER BY tv_shows.title;
