2c2e2ee8f0ac2a45d25730cf8be280d3b3b32f61
N-Steph <snouaffo@gmail.com>
2024-02-28 14:02:55 +0000

    printing title of movie based on movie id

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,10 +2,10 @@
 
 const request = require('request');
 
-const starWarUrl = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
-request(starWarUrl, function (error, response, body) {
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
   if (!error && response.statusCode === 200) {
-    const info = JSON.parse(body);
-    console.log(info.title);
+    const jsonString = JSON.parse(body);
+    console.log(jsonString.title);
   }
 });
