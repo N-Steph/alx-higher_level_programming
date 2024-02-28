e813185fb222300d86628fdf7e395e1d19bca1cf
N-Steph <snouaffo@gmail.com>
2024-02-28 14:29:09 +0000

    storing the result of a result in a file

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+request(url, function (error, response, data) {
+  if (!error && response.statusCode === 200) {
+    fs.writeFile(filePath, data, { encoding: ' utf-8' }, function (error) {
+      if (error) {
+        console.log(error);
+      }
+    });
+  }
+});
