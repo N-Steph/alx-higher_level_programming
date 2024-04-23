b535fa1c956fd93357d4b5655fde675239e68419
N-Steph <snouaffo@gmail.com>
2024-04-23 17:43:01 +0000

    print the status code of a get request

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const process = require('process');
+const request = require('request');
+
+request.get(process.argv[2]).on('response', (response) => {
+  console.log('code: ', response.statusCode);
+});
