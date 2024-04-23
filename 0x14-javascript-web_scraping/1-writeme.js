92f2031a7b9626359dc499c4dd73b0c91a5172c4
N-Steph <119834045+N-Steph@users.noreply.github.com>
2024-04-23 14:51:50 +0100

    Delete 0x14-javascript-web_scraping directory

--- 0x14-javascript-web_scraping/1-writeme.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-const fs = require('fs');
-
-try {
-  fs.writeFileSync(process.argv[2], process.argv[3]);
-} catch (e) {
-  console.log(e);
-}
--- 0x14-javascript-web_scraping/README.md
+++
@@ -1,2 +0,0 @@
-## Description
-In this directory, we are going to learn the basics of web scraping in JavaScript
