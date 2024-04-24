6e0312e90cf5bea841e930988378b4d03960f32f
N-Steph <snouaffo@gmail.com>
2024-04-24 09:46:55 +0000

    correct the callback function of writeFile function

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -6,8 +6,10 @@ const request = require('request');
 
 request(process.argv[2], (error, response, body) => {
   if (response.statusCode === 200) {
-    fs.writeFile(process.argv[3], body.toString(), (err) => {
-      console.log(err);
+    fs.writeFile(process.argv[3], body.toString(), err => {
+      if (err) {
+        console.log(err);
+      }
     });
   } else {
     console.log(error);
