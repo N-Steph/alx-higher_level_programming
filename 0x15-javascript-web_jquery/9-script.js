c74c73db16a9d253b57faf762445109a10bd4fd6
N-Steph <snouaffo@gmail.com>
2024-05-02 14:31:15 +0000

    script always load after the html document is parse

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,3 +1,5 @@
-$.get('https://hellosalut.stefanbohacek.dev/?lang=fr', (response) => {
-  $('DIV#hello').text(response.hello);
+$(window).on('load', function () {
+  $.get('https://hellosalut.stefanbohacek.dev/?lang=fr', (response) => {
+    $('DIV#hello').text(response.hello);
+  });
 });
